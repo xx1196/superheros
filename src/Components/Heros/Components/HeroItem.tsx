1c8d1516903c4d825025876af1b359ceb21a5183
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2021-07-27 08:27:43 -0500

    *Se cmbia esilo al mostrar los heroes

--- src/Components/Heros/Components/HeroItem.tsx
+++ src/Components/Heros/Components/HeroItem.tsx
@@ -1,42 +1,57 @@
 import {FC} from "react";
 import {HeroItemProps} from "../../../Interfaces/PropsInterfaces";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
-import ListItem from "@material-ui/core/ListItem";
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
+import {Card, CardActionArea, CardActions, CardContent, CardMedia} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        inline: {
-            display: 'inline',
-        },
-    }),
-);
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
 
 export const HeroItem: FC<HeroItemProps> = ({hero}) => {
     const classes = useStyles();
 
-    return <ListItem key={hero.id} alignItems="flex-start">
-        <ListItemAvatar>
-            <Avatar/>
-        </ListItemAvatar>
-        <ListItemText
-            primary={hero.superhero}
-            secondary={
-                <>
-                    <Typography
-                        component="span"
-                        variant="body2"
-                        className={classes.inline}
-                        color="textPrimary"
-                    >
-                        {hero.alter_ego}
-                    </Typography>
-                    {" " + hero.first_appearance}
-                </>
-            }
-        />
-    </ListItem>
+    return <Card className={classes.root}>
+        <CardActionArea>
+            <CardMedia
+                component="img"
+                alt={hero.superhero}
+                height="140"
+                image={`assets/heroes/${hero.id}.jpg`}
+                title={hero.superhero}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {hero.superhero}
+                </Typography>
+                <Typography variant="body1" color="textSecondary" component="p">
+                    Aparece por primera vez en <Typography color="primary"
+                                                           component="p">{hero.first_appearance}</Typography> bajo el
+                    alter ego
+                    de <Typography color="primary" component="p">{hero.alter_ego}</Typography>
+                </Typography>
+
+                {
+                    (hero.alter_ego !== hero.characters) &&
+                    <>
+                        <Typography component="h2">
+                            Otras apariciones como:
+                        </Typography>
+
+                        <Typography variant="body2" color="secondary" component="p">
+                            {hero.characters}
+                        </Typography>
+                    </>
+                }
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary">
+                Ver mas...
+            </Button>
+        </CardActions>
+    </Card>
 }
--- src/Components/Heros/Components/HeroList.tsx
+++ src/Components/Heros/Components/HeroList.tsx
@@ -3,33 +3,19 @@ import {FC} from "react";
 import {HeroListProps} from "../../../Interfaces/PropsInterfaces";
 import {Hero} from "../../../Interfaces/ModelsInterfaces";
 import * as PropTypes from 'prop-types'
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
 import {HeroItem} from "./HeroItem";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            width: '100%',
-            maxWidth: '36ch',
-            backgroundColor: theme.palette.background.paper,
-        },
-    }),
-);
-
-
 export const HeroList: FC<HeroListProps> = ({publisher}) => {
-    const classes = useStyles();
 
     const heroes: Hero[] = GetHeroesByPublisher(publisher);
 
-    return <List className={classes.root}>
+    return <>
         {
             heroes.map((hero: Hero) => (
                 <HeroItem hero={hero}/>
             ))
         }
-    </List>
+    </>
 }
 
 HeroList.propTypes = {
